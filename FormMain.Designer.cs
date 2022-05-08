b78bc64f4f9286755f78618410939080ff9820d4
Hassan Elgamily <hassanelgamily@gmail.com>
2022-05-08 23:21:45 +0200

    Add project files.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace OSM
+{
+    partial class FormOSM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.tableLayoutPanelInput = new System.Windows.Forms.TableLayoutPanel();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.progressBarOSM = new System.Windows.Forms.ProgressBar();
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.tableLayoutPanelInput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.ColumnCount = 3;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.87636F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.5635F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.56014F));
+            this.tableLayoutPanelMain.Controls.Add(this.buttonExport, 1, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.buttonClose, 2, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.tableLayoutPanelInput, 0, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.progressBarOSM, 0, 1);
+            this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 2;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.70968F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.29032F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(574, 124);
+            this.tableLayoutPanelMain.TabIndex = 1;
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExport.Location = new System.Drawing.Point(363, 82);
+            this.buttonExport.Margin = new System.Windows.Forms.Padding(3, 3, 8, 9);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(95, 33);
+            this.buttonExport.TabIndex = 1;
+            this.buttonExport.Text = "Export";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.Location = new System.Drawing.Point(469, 82);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(3, 3, 8, 9);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(97, 33);
+            this.buttonClose.TabIndex = 2;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // tableLayoutPanelInput
+            // 
+            this.tableLayoutPanelInput.ColumnCount = 2;
+            this.tableLayoutPanelMain.SetColumnSpan(this.tableLayoutPanelInput, 3);
+            this.tableLayoutPanelInput.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.63941F));
+            this.tableLayoutPanelInput.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 87.3606F));
+            this.tableLayoutPanelInput.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 62F));
+            this.tableLayoutPanelInput.Controls.Add(this.labelInput, 0, 0);
+            this.tableLayoutPanelInput.Controls.Add(this.textBoxInput, 1, 0);
+            this.tableLayoutPanelInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelInput.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanelInput.Name = "tableLayoutPanelInput";
+            this.tableLayoutPanelInput.RowCount = 1;
+            this.tableLayoutPanelInput.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelInput.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 73F));
+            this.tableLayoutPanelInput.Size = new System.Drawing.Size(568, 73);
+            this.tableLayoutPanelInput.TabIndex = 0;
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInput.Location = new System.Drawing.Point(3, 0);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(65, 73);
+            this.labelInput.TabIndex = 0;
+            this.labelInput.Text = "Input";
+            this.labelInput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInput.Location = new System.Drawing.Point(74, 25);
+            this.textBoxInput.Margin = new System.Windows.Forms.Padding(3, 25, 6, 3);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ReadOnly = true;
+            this.textBoxInput.Size = new System.Drawing.Size(488, 26);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.Text = "http://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf";
+            // 
+            // progressBarOSM
+            // 
+            this.progressBarOSM.Location = new System.Drawing.Point(19, 94);
+            this.progressBarOSM.Margin = new System.Windows.Forms.Padding(19, 15, 3, 3);
+            this.progressBarOSM.Name = "progressBarOSM";
+            this.progressBarOSM.Size = new System.Drawing.Size(121, 13);
+            this.progressBarOSM.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBarOSM.TabIndex = 0;
+            this.progressBarOSM.Visible = false;
+            // 
+            // FormOSM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 124);
+            this.ControlBox = false;
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.MaximumSize = new System.Drawing.Size(590, 163);
+            this.MinimumSize = new System.Drawing.Size(590, 163);
+            this.Name = "FormOSM";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OSM";
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.tableLayoutPanelInput.ResumeLayout(false);
+            this.tableLayoutPanelInput.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelInput;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.ProgressBar progressBarOSM;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,107 @@
+﻿using OsmSharp.Osm.PBF.Streams;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace OSM
+{
+    public partial class FormOSM : Form
+    {
+        private List<string> postalCodeList = new List<string>();
+
+        public FormOSM()
+        {
+            InitializeComponent();
+        }
+
+        private string DownloadFile(string address)
+        {
+            WebClient client = new WebClient();
+            string filePath = Path.GetTempFileName() + ".osm.pbf";
+            client.DownloadFile(address, filePath);
+
+            return filePath;
+        }
+
+        private void WriteToTextFile(string filePath)
+        {
+            File.WriteAllLines(filePath, postalCodeList.ToArray());
+        }
+
+        private void Export()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "OSM";
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //download OSM.PBF file
+                    string filePath = DownloadFile(textBoxInput.Text);
+
+                    //read OSM.PBF file
+                    using (var fileStream = new FileInfo(@filePath).OpenRead())
+                    {
+                        var source = new PBFOsmStreamSource(fileStream);
+
+                        progressBarOSM.Visible = true;
+
+                        int i = 0;
+                        foreach (var element in source)
+                        {
+                            //get Postal_Code(s)
+                            string s_element = element.ToString();
+                            string[] sa_element = s_element.Split(',');
+                            string s_postalCode = Array.Find(sa_element, e => e.StartsWith("postal_code", StringComparison.Ordinal));
+
+                            if (s_postalCode != null)
+                            {
+                                string[] sa_postalCode = s_postalCode.Split('=');
+
+                                if (sa_postalCode.Length == 2 && !postalCodeList.Contains(sa_postalCode[1]))
+                                {
+                                    //fill postalCodeList
+                                    postalCodeList.Add(sa_postalCode[1]);
+                                }
+                            }
+
+                            //refresh progressbar
+                            if (i == 1000)
+                            {
+                                progressBarOSM.Refresh();
+                                i = 0;
+                            }
+
+                            i++;
+                        }
+
+                        progressBarOSM.Visible = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    progressBarOSM.Visible = false;
+                }
+
+                //write to .txt file
+                WriteToTextFile(saveFileDialog.FileName);
+
+                Close();
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            Export();
+        }
+    }
+}
